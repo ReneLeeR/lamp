d125d4e1886725c945a5e7661f297ccd75d16e5d
ReneLeeR <develop@mail.ita.mx>
2017-06-26 17:36:33 -0500

    new

--- nodejs/itworks.js
+++ nodejs/itworks.js
@@ -1,49 +1,19 @@
+var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var fs = require('fs');
-var options = {
-  key: fs.readFileSync('/var/www/cgi/nodejs/ssl/node.key'),
-  cert: fs.readFileSync('/var/www/cgi/nodejs/ssl/node.crt')
-};
-http.createServer(function(request, response) {  
-  fs.readFile((request.url.charAt(request.url.length-1) == "/") ? "/var/www/html"+request.url+"index.html" : "/var/www/html" + request.url, function (error, pgResp) 
-  {
-    console.log(request.url);
-    if(error)
-    {
-      fs.readFile("/var/www/html/404/index.html",function(error, notfound)
-      {
-        response.writeHead(200);
-        response.write(notfound);
-        response.end();
-      });
-    }
-    else
-    {
-      response.writeHead(200);
-      response.write(pgResp);
-      response.end();
-    }
-  });
-}).listen(80);
-https.createServer(options, function(request, response) {
-  fs.readFile((request.url.charAt(request.url.length-1) == "/") ? "/var/www/html"+request.url+"index.html" : "/var/www/html" + request.url, function (error, pgResp) 
-  {
-    console.log(request.url);
-    if(error)
-    {
-      fs.readFile("/var/www/html/404/index.html",function(error, notfound)
-      {
-        response.writeHead(200);
-        response.write(notfound);
-        response.end();
-      });
-    }
-    else
-    {
-      response.writeHead(200);
-      response.write(pgResp);
-      response.end();
-    }
-  });
-}).listen(443);
+var credentials = {key: fs.readFileSync('/var/www/cgi/nodejs/ssl/node.key', 'utf8'), cert: fs.readFileSync('/var/www/cgi/nodejs/ssl/node.crt', 'utf8')};
+var express = require('express');
+var app = express();
+
+app.use(express.static("/var/www/html"));
+app.get('/', function(req,res) {
+    res.send(fs.readFileSync('/var/www/html/index.html', 'utf8'));
+});
+
+
+
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80);
+httpsServer.listen(443);
